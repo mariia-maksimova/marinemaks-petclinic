4c945cf9ff940b778cbafdca0850ee3e213a2df2
Mariia Maksimova <mariia.maksimova@transferwise.com>
2023-12-31 16:12:25 +0000

    Add Contact Info to Owner. Closes #28
    Signed-off-by: Mariia Maksimova <mariia.maksimova@transferwise.com>

--- petclinic-data/src/main/java/mm/model/persons/owners/Owner.java
+++ petclinic-data/src/main/java/mm/model/persons/owners/Owner.java
@@ -1,12 +1,32 @@
 package mm.model.persons.owners;
 
+import mm.model.Address;
 import mm.model.persons.Person;
 import mm.model.pets.Pet;
 
 import java.util.Set;
 
 public class Owner extends Person {
+
+    private Address address;
+    private String phoneNumber;
     private Set<Pet> pets;
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 
     public Set<Pet> getPets() {
         return pets;
