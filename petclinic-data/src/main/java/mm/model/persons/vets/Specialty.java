7e06cf87747f6c554ad94014c0fca700806341c3
Mariia Maksimova <mariia.maksimova@transferwise.com>
2024-01-07 19:09:06 +0000

    Convert Vets to JPA Entities. Closes #37
    Signed-off-by: Mariia Maksimova <mariia.maksimova@transferwise.com>

--- petclinic-data/src/main/java/mm/model/persons/vets/Specialty.java
+++ petclinic-data/src/main/java/mm/model/persons/vets/Specialty.java
@@ -1,8 +1,15 @@
 package mm.model.persons.vets;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import mm.model.BaseEntity;
 
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
+
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- petclinic-data/src/main/java/mm/model/persons/vets/Vet.java
+++ petclinic-data/src/main/java/mm/model/persons/vets/Vet.java
@@ -1,11 +1,19 @@
 package mm.model.persons.vets;
 
+import jakarta.persistence.*;
 import mm.model.persons.Person;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vets_specialties",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
