4ca4a8085146928729ded67905722842c1869b67
Mariia Maksimova <mariia.maksimova@transferwise.com>
2024-01-07 20:02:27 +0000

    Add Spring Data JPA Pet Service. Closes #43
    Signed-off-by: Mariia Maksimova <mariia.maksimova@transferwise.com>

---
+++ petclinic-data/src/main/java/mm/infrastructure/services/springdatajpa/PetSPJpaService.java
@@ -0,0 +1,44 @@
+package mm.infrastructure.services.springdatajpa;
+
+import lombok.AllArgsConstructor;
+import mm.domain.pets.Pet;
+import mm.infrastructure.repositories.PetRepository;
+import mm.infrastructure.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@AllArgsConstructor
+@Profile("springdatajpa")
+public class PetSPJpaService implements PetService {
+    private final PetRepository petRepository;
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
