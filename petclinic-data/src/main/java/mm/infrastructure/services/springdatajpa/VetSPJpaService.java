de875d4dd55ff20b9dbe4bcdf31bd8ec2ad0d100
Mariia Maksimova <mariia.maksimova@transferwise.com>
2024-01-07 19:43:24 +0000

    Add Spring Data JPA Vet Service. Closes #41
    Signed-off-by: Mariia Maksimova <mariia.maksimova@transferwise.com>

---
+++ petclinic-data/src/main/java/mm/infrastructure/services/springdatajpa/VetSPJpaService.java
@@ -0,0 +1,47 @@
+package mm.infrastructure.services.springdatajpa;
+
+import lombok.AllArgsConstructor;
+import mm.domain.persons.owners.Owner;
+import mm.domain.persons.vets.Vet;
+import mm.infrastructure.repositories.VetRepository;
+import mm.infrastructure.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@AllArgsConstructor
+@Profile("springdatajpa")
+public class VetSPJpaService extends VetService {
+
+    private final VetRepository vetRepository;
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
