2458617a52f40eba3d0bb5ea0e7047958c327009
Mariia Maksimova <mariia.maksimova@transferwise.com>
2024-01-07 20:04:34 +0000

    Add Spring Data JPA Vet Speciality Service. Closes #44
    Signed-off-by: Mariia Maksimova <mariia.maksimova@transferwise.com>

---
+++ petclinic-data/src/main/java/mm/infrastructure/services/springdatajpa/SpecialtySPJpaService.java
@@ -0,0 +1,46 @@
+package mm.infrastructure.services.springdatajpa;
+
+import lombok.AllArgsConstructor;
+import mm.domain.persons.vets.Specialty;
+import mm.infrastructure.repositories.SpecialtyRepository;
+import mm.infrastructure.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@AllArgsConstructor
+@Profile("springdatajpa")
+public class SpecialtySPJpaService implements SpecialtyService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return specialtyRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialtyRepository.deleteById(id);
+    }
+}
