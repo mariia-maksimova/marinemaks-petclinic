2e9833ce736fafde96e89d56064c523ad99c9d9f
Mariia Maksimova <mariia.maksimova@transferwise.com>
2024-01-07 20:29:25 +0000

    Add Spring Data JPA Visit Service. Closes #47
    Signed-off-by: Mariia Maksimova <mariia.maksimova@transferwise.com>

---
+++ petclinic-data/src/main/java/mm/infrastructure/services/springdatajpa/VisitSPJpaService.java
@@ -0,0 +1,46 @@
+package mm.infrastructure.services.springdatajpa;
+
+import lombok.AllArgsConstructor;
+import mm.domain.persons.vets.Vet;
+import mm.domain.visits.Visit;
+import mm.infrastructure.repositories.VisitRepository;
+import mm.infrastructure.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@AllArgsConstructor
+@Profile("springdatajpa")
+public class VisitSPJpaService implements VisitService {
+    private final VisitRepository visitRepository;
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
